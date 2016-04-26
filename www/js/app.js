1c70592e6ba1328c5b4c63e31e4a0383ebaffb69
dominiqueh <dominiquehorner@gmail.com>
2016-04-26 11:35:28 -0600

    app is up, photo isn't yet being taken

--- www/js/app.js
+++ www/js/app.js
@@ -6,12 +6,88 @@
 var oBapp = angular.module('starter', ['ionic', 'ngCordova'])
 // var db = new  --- later for database
 
+// oneBoard.config(function($stateProvider, $urlRouterProvider) {
+  // $stateProvider
+    // .state("login", {
+    //   url: "/login",
+    //   templateUrl: "templates/login.html",
+    //   controller: "loginController",
+    //   cache: false
+    // })
+    // .state("secure", {
+    //   url: "/secure",
+    //   templateUrl: "templates/secure.html",
+    //   controller: "secureController"
+    // });
+    // $urlRouterProvider.otherwise("/secure");
+// });
+// login authentication
+// oBapp.controller("loginController", function($scope, $state, $mongoAuth) {
+//   var dbAuth = $fire
+// })
+
+
+// this appears to take photos :D
+oBapp.controller('PictureCtrl', function($scope, $ionicHistory, $cordovaCamera) {
+  $scope.pictureUrl = 'http:placeholder.it/200x200';
+  $ionicHistory.clearHistory();
+  // $scope.images = [];
+
+  // var fbAuth = fb.getAuth();
+  // if(fbAuth){
+  //   var userReference = fb.child("users/" + fbAuth.uid);
+  //   var syncArray = $firebaseArray(userReference.child("images"));
+  //   $scope.images = syncArray;
+  // }
+  //   else{
+  //     $state.go("login");
+  //   }
+  // }
+$scope.takeImage("deviceready", function(){
+  // $scope.takeImage = function() {
+    console.log("I am running.. Fingers crossed!")
+      var options = {
+          quality: 80,
+          destinationType: Camera.DestinationType.DATA_URL,
+          sourceType: Camera.PictureSourceType.CAMERA,
+          allowEdit: true,
+          encodingType: Camera.EncodingType.JPEG,
+          targetWidth: 500,
+          targetHeight: 500,
+          popoverOptions: CameraPopoverOptions,
+          saveToPhotoAlbum: false,
+            correctOrientation: true
+      };
+
+      $cordovaCamera.getPicture(options).then(function(imageData) {
+          var image = $scope.getElementById('myImage');
+          image.src = "data:image/jpeg;base64," + imageData;
+      }, function(err) {
+        console.log(err)
+      });
+  }, false);
+// Retrieve Image File Location
+  document.getImageFile("deviceready", function () {
+    var options = {
+      destinationType: Camera.DestinationType.FILE_URI,
+      sourceType: Camera.PictureSourceType.CAMERA,
+    };
+    $cordovaCamera.getPicture(options).then(function(imageURI) {
+      var image = $scope.getElementById('myImage');
+      image.src = imageURI;
+    }, function(err) {
+      // error
+    });
+    $cordovaCamera.cleanup().then(...); // only for FILE_URI
+  }, false);
+});
+
 oBapp.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      // cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
       // Don't remove this line unless you know what you are doing. It stops the viewport
       // from snapping when text inputs are focused. Ionic handles this internally for
@@ -23,40 +99,3 @@ oBapp.run(function($ionicPlatform) {
     }
   });
 })
-imageApp.config(function($stateProvider, $stateRouterProvider) {
-  $stateProvider
-    .state("login", {
-      url: "/login",
-      templateUrl: "templates/login.html"
-      controller: "loginController"
-      cache: false
-    })
-    .state("secure", {
-      url: "/secure",
-      templateUrl: "templates/secure.html",
-      controller: "SecureController"
-    });
-    $urlRouterProvider.otherwise("/login");
-});
-// this appears to take photos :D
-oBapp.controller('MainCtrl', function($scope, $cordovaCamera) {
-  $scope.takeImage = function() {
-      var options = {
-          quality: 80,
-          destinationType: Camera.DestinationType.DATA_URL,
-          sourceType: Camera.PictureSourceType.CAMERA,
-          allowEdit: true,
-          encodingType: Camera.EncodingType.JPEG,
-          targetWidth: 250,
-          targetHeight: 250,
-          popoverOptions: CameraPopoverOptions,
-          saveToPhotoAlbum: false
-      };
-
-      $cordovaCamera.getPicture(options).then(function(imageData) {
-          $scope.srcImage = "data:image/jpeg;base64," + imageData;
-      }, function(err) {
-          // error
-      });
-  }
-});
